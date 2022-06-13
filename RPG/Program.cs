fba8919bb396ca0d846498facf5ace1b91f5d1cd
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-06-12 22:01:12 -0300

    RPG V6

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.IO;
 
 namespace RPG
 {
@@ -8,28 +9,52 @@ namespace RPG
         static void Main(string[] args)
         {
             var rand = new Random();
-            var listaPj = new List<Personaje>(); //Agrego al primer personaje
-            var carac = new Caracteristicas();
-            var datos = new Datos();
-            var pj = new Personaje(carac, datos);
-            listaPj.Add(pj);
 
-            Console.WriteLine("\nEl número de peleas se designará aleatoriamente entre 1 y 10. \nEn las peleas, cada atacante podrá atacar y defender 3 veces, al finalizar cada uno con sus acciones. \nGanará aquel que posea más vida, si ambos luchadores poseen la misma cantidad de vida, la misma se repetirá hasta que uno de los caiga");
+            do
+            {
 
-            int peleas = rand.Next(1, 11), empate = 0; //Ingreso la cantidad de peleas que habrá
-            Console.WriteLine($"\nCantidad de peleas que habrá: {peleas}");
+                Console.WriteLine("\n¿Desea ver la lista de ganadores previos, llevar a cabo unas batallas, o finalizar el programa? \n\"L\" para ver la lista, \"F\" para finalizar, o cualquier otro caracter para pelear");
+                char pel = Console.ReadKey().KeyChar;
 
-            ProcesoDePelea(rand, listaPj, ref carac, ref datos, ref pj, ref peleas, ref empate);
+                if (pel == 'l' || pel == 'L')
+                {
+                    var leer = new StreamReader(File.Open("../ganadores.csv", FileMode.Open));
+                    Console.WriteLine($"\n{leer.ReadToEnd()}");
+                    leer.Close();
+                }
+                else
+                {
+                    if (pel == 'f' || pel == 'F')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        var listaPj = new List<Personaje>(); //Agrego al primer personaje
+                        var carac = new Caracteristicas();
+                        var datos = new Datos();
+                        var pj = new Personaje(carac, datos);
+                        listaPj.Add(pj);
 
-            Ganador(listaPj);
+                        Console.WriteLine("\nEl número de peleas se designará aleatoriamente entre 1 y 10. \nEn las peleas, cada atacante podrá atacar y defender 3 veces, al finalizar cada uno con sus acciones. \nGanará aquel que posea más vida, si ambos luchadores poseen la misma cantidad de vida, la misma se repetirá hasta que uno de los caiga");
+
+                        int peleas = rand.Next(1, 11), empate = 0; //Ingreso la cantidad de peleas que habrá
+                        Console.WriteLine($"\nCantidad de peleas que habrá: {peleas}");
+
+                        ProcesoDePelea(rand, listaPj, ref carac, ref datos, ref pj, ref peleas, ref empate);
+
+                        Ganador(listaPj);
+                    }
+                }
+            } while (true);
         }
 
         private static void Ganador(List<Personaje> listaPj)
         {
             Console.WriteLine("\n//--------------------Ganador definitivo--------------------//"); //Muestro al personaje que logró sobrevivir
-            Console.WriteLine($"Tipo de personaje: {listaPj[0].Dat.Tipo}");
             Console.WriteLine($"Nombre del personaje: {listaPj[0].Dat.Nombre}");
             Console.WriteLine($"Apodo del personaje: {listaPj[0].Dat.Apodo}");
+            Console.WriteLine($"Tipo de personaje: {listaPj[0].Dat.Tipo}");
             Console.WriteLine($"Fecha de nacimiento: {listaPj[0].Dat.FechaNac}");
             Console.WriteLine($"Edad: {listaPj[0].Dat.Edad}");
             Console.WriteLine($"Salud: {listaPj[0].Dat.Salud}");
@@ -38,6 +63,11 @@ namespace RPG
             Console.WriteLine($"Fuerza: {listaPj[0].Car.Fuerza}");
             Console.WriteLine($"Nivel: {listaPj[0].Car.Nivel}");
             Console.WriteLine($"Armadura: {listaPj[0].Car.Armadura}");
+
+            var escribir = new StreamWriter(File.Open("../ganadores.csv", FileMode.Append));
+            string texto = $"Nombre: {listaPj[0].Dat.Nombre}, Apodo: {listaPj[0].Dat.Apodo}, Tipo de personaje: {listaPj[0].Dat.Tipo}, Fecha de nacimiento: {listaPj[0].Dat.FechaNac}, Momento en el que se llevó la batalla: {DateTime.Now}";
+            escribir.WriteLine(texto);
+            escribir.Close();
         }
 
         private static void ProcesoDePelea(Random rand, List<Personaje> listaPj, ref Caracteristicas carac, ref Datos datos, ref Personaje pj, ref int peleas, ref int empate)
@@ -60,9 +90,9 @@ namespace RPG
                 Console.WriteLine("\n\nPersonajes a luchar:"); //Muestro la información relevante de los personajes a luchar
                 foreach (var item in listaPj)
                 {
-                    Console.WriteLine($"\nTipo de personaje: {item.Dat.Tipo}");
-                    Console.WriteLine($"Nombre del personaje: {item.Dat.Nombre}");
+                    Console.WriteLine($"\nNombre del personaje: {item.Dat.Nombre}");
                     Console.WriteLine($"Apodo del personaje: {item.Dat.Apodo}");
+                    Console.WriteLine($"Tipo de personaje: {item.Dat.Tipo}");
                 }
 
                 Console.WriteLine("\nPresione para iniciar la pelea"); //Ingresar algo para iniciar la pelea, dando tiempo para ver la información de los personajes
@@ -138,6 +168,12 @@ namespace RPG
                 float PDEF = defensor.Car.Armadura * defensor.Car.Velocidad; //Poder de defensa
                 float MDP = 50000; //Máximo daño provocable
                 int DP = Convert.ToInt32((((VA * ED) - PDEF) / MDP) * 5); //Daño provocado
+                /*
+                ¡¡¡ATENCIÓN!!! Se decidió cambiar un cálculo en la fórmula de ataque.
+                En la última operación se multiplicaba por 100, y eso provocaba que el daño resultante sea
+                mayor a 100, es decir, mataba al contrincante de un hit, por lo que se lo cambió por 5,
+                un valor no tan grande que, sigue provocando daños mayores a 100, pero ya muy infrecuente ;)
+                */
 
                 Console.WriteLine($"Daño ejercido por {atacante.Dat.Apodo}: {DP}");
                 defensor.Dat.Salud -= DP;
